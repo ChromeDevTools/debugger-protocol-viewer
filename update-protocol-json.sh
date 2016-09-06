cd3c03895217626519ecf655936e4bf9f5d5c3f3
Paul Irish <paul.irish@gmail.com>
2016-09-06 12:09:35 -0700

    Show latest protocol versions, split between browser & js

--- update-protocol-json.sh
+++ update-protocol-json.sh
@@ -2,8 +2,8 @@
 set -x
 
 chromium_src_path="$HOME/chromium/src"
-git -C "$chromium_src_path" fetch origin
-git -C "$chromium_src_path" checkout origin/master
+#git -C "$chromium_src_path" fetch origin
+#git -C "$chromium_src_path" checkout origin/master
 
 browser_protocol_path="$chromium_src_path/third_party/WebKit/Source/core/inspector/browser_protocol.json"
 js_protocol_path="$chromium_src_path/third_party/WebKit/Source/platform/v8_inspector/js_protocol.json"
@@ -25,13 +25,20 @@ node create-search-index.js
 
 # get the latest change
 cd $(dirname "$browser_protocol_path")
-commit_line=$(git log --no-color HEAD~1..HEAD  | grep -E -o "^commit.*")
-date_line=$(git log --no-color HEAD~1..HEAD  | grep -E -o "^Date.*")
-commit_hash=$(echo $commit_line | grep -E -o "[0-9a-f]{20,80}")
+br_commit_line=$(git log --no-color --max-count=1 -- . | grep -E -o "^commit.*")
+br_date_line=$(git log --no-color --max-count=1 -- . | grep -E -o "^Date.*")
+br_commit_hash=$(echo $br_commit_line | grep -E -o "[0-9a-f]{20,80}")
+
+cd $(dirname "$js_protocol_path")
+js_commit_line=$(git log --no-color --max-count=1 -- . | grep -E -o "^commit.*")
+js_date_line=$(git log --no-color --max-count=1 -- . | grep -E -o "^Date.*")
+js_commit_hash=$(echo $js_commit_line | grep -E -o "[0-9a-f]{20,80}")
 
 # copy it into the HTML file
 cd $local_script_path
-cat _versions/tot.html | sed -e "s/^Date.*/$date_line/" | sed -E "s/[0-9a-f]{20,80}/$commit_hash/" > _versions/tot.html.new
+
+cat _versions/tot.html | sed -Ee "s/^(<code browser>)Date.*/\1$br_date_line/" | sed -Ee "s/(browser.*)([0-9a-f]{40,80})/\1$br_commit_line/" > _versions/tot.html.new
+cat _versions/tot.html |      sed -Ee "s/^(<code js>)Date.*/\1$js_date_line/"      | sed -Ee "s/(js.*)([0-9a-f]{40,80})/\1$js_commit_hash/" > _versions/tot.html.new
 mv _versions/tot.html.new _versions/tot.html
 
 
