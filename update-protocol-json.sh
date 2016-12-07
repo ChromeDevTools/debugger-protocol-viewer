2dbad09ac5a9dda56e0a69f10a6242f9df49f945
Paul Irish <paul.irish@gmail.com>
2016-12-07 15:26:38 -0800

    improve protocol updating script.

--- update-protocol-json.sh
+++ update-protocol-json.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 set -x
 
-chromium_src_path="$HOME/chromium/src"
-git -C "$chromium_src_path" fetch origin
-git -C "$chromium_src_path" checkout origin/master
-env GYP_DEFINES=disable_nacl=1 gclient sync --jobs=16
+chromium_src_path="$HOME/chromium-tot/src"
+local_script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# => into chromium
+cd $chromium_src_path;
+
+# check for what branch we're on. 
+origBranchName="$(git symbolic-ref --quiet --short HEAD 2> /dev/null || \
+    git describe --all --exact-match HEAD 2> /dev/null || \
+    git rev-parse --short HEAD 2> /dev/null)";
+
+git fetch origin
+git checkout -f origin/master
+env GYP_DEFINES=disable_nacl=1 gclient sync --jobs=70
 
 browser_protocol_path="$chromium_src_path/third_party/WebKit/Source/core/inspector/browser_protocol.json"
 js_protocol_path="$chromium_src_path/v8/src/inspector/js_protocol.json"
 
-local_script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# => into viewer
+cd $local_script_path
 local_tot_protocol_path="_data/tot/protocol.json"
 local_v8_protocol_path="_data/v8/protocol.json"
 
@@ -18,13 +29,13 @@ if ! [ -s $browser_protocol_path ]; then
 fi
 # copy the protocol.json over
 cp $js_protocol_path $local_v8_protocol_path
+# merge and create all our data files
 node merge-protocol-files.js $browser_protocol_path $js_protocol_path > $local_tot_protocol_path
-
-cd $local_script_path
 node create-domain-files.js
 node create-search-index.js
 
 # get the latest change
+# => into chromium
 cd $(dirname "$browser_protocol_path")
 br_commit_line=$(git log --no-color --max-count=1 -- browser_protocol.json | grep -E -o "^commit.*")
 br_date_line=$(git log --no-color --max-count=1 -- browser_protocol.json | grep -E -o "^Date.*")
@@ -36,11 +47,11 @@ js_date_line=$(git log --no-color --max-count=1 -- js_protocol.json | grep -E -o
 js_commit_hash=$(echo $js_commit_line | grep -E -o "[0-9a-f]{20,80}")
 
 # copy it into the HTML file
+# => into viewer
 cd $local_script_path
 
 cat _versions/tot.html | sed -Ee "s/^(<code browser>)Date.*/\1$br_date_line/" | sed -Ee "s/(browser.*)([0-9a-f]{40,80})/\1$br_commit_line/" > _versions/tot.html.new
-cat _versions/tot.html |      sed -Ee "s/^(<code js>)Date.*/\1$js_date_line/"      | sed -Ee "s/(js.*)([0-9a-f]{40,80})/\1$js_commit_hash/" > _versions/tot.html.new
-cp _versions/tot.html.new _versions/tot.html
-rm -f cp _versions/tot.html.new
+cat _versions/tot.html.new | sed -Ee "s/^(<code js>)Date.*/\1$js_date_line/"  | sed -Ee "s/(js.*)([0-9a-f]{40,80})/\1$js_commit_hash/" > _versions/tot.html
+rm -f _versions/tot.html.new
 
 set +x
