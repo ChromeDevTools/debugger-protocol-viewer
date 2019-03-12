aa02eabce5bbeb9822b4203dd52ffecf7b1c92ce
Paul Irish <paul.irish@gmail.com>
2019-03-12 16:24:09 -0700

    sort and tag leftnav with experimental/deprecated

--- generate-sidenav-html.js
+++ generate-sidenav-html.js
@@ -1,28 +1,34 @@
 'use strict';
 
 
-const fs = require('fs');
-
-debugger;
-
-
 const verSlugs = require('./_data/versions.json').map(e => e.slug);
-
-const getDomainsInVersion = protocol => tot.domains.map(d => d.domain);
 
 const allDomains = {};
 
 for (const slug of verSlugs){
   const protocol = require(`./_data/${slug}/protocol.json`);
-  const domains = protocol.domains.map(d => d.domain);
+  const domains = protocol.domains;
   domains.forEach(domain => {
-    const item = allDomains[domain] || [];
-    item.push(slug);
-    allDomains[domain] = item;
+    const id = domain.domain;
+    const tags = allDomains[id] || [];
+    tags.push(slug);
+
+    // Annotate with tot's experimental / deprecated status
+    if (slug === 'tot') {
+      if (domain.experimental) tags.push('experimental');
+      if (domain.deprecated) tags.push('deprecated');
+    }
+    allDomains[id] = tags;
   });
 }
 
-const str = Object.entries(allDomains).sort(([domainA], [domainB]) => domainA.localeCompare(domainB))
+const str = Object.entries(allDomains).sort(([domainA, tagsA], [domainB, tagsB]) => {
+    const isExpOrDepr = a => a === 'experimental' || a === 'deprecated';
+    const getTagsStr = tags => tags.filter(isExpOrDepr).join('');
+    const tagSortResult = getTagsStr(tagsA).localeCompare(getTagsStr(tagsB));
+    if (tagSortResult !== 0) return tagSortResult;
+    return domainA.localeCompare(domainB);
+  })
   .map(([id, versions]) => `<a data-title="${id}" class="${versions.join(' ')}">${id}</a>`)
   .join('\n');
 
