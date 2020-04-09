427b50ca96e974373e30f0fa8cac8f5ae551c8d9
Paul Irish <commits@paul.irish>
2020-04-09 16:11:14 -0700

    no-pwa

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -3,12 +3,12 @@
 module.exports = {
   ci: {
     collect: {
-      numberOfRuns: 5,
+      numberOfRuns: 3, // ignored due to https://github.com/treosh/lighthouse-ci-action/issues/48
       startServerCommand: "npm run serve",
       startServerReadyPattern: "Served by",
     },
     assert: {
-      preset: "lighthouse:recommended",
+      preset: "lighthouse:no-pwa",
     }
   },
 };
